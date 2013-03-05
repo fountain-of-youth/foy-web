ba87642bdd7d6fd70336488a99acea308ade7728
Roberto Soares <roberto.tech@gmail.com>
2013-03-05 15:18:26 -0300

    added project parser runner

---
+++ lib/foy/runner/project_parser.rb
@@ -0,0 +1,32 @@
+require 'foy/git_fetcher'
+require 'foy/handler_caller'
+
+module Foy
+  module Runner
+    class ProjectParser
+      class << self
+        def run
+          Project.all.each do |project|
+            handler = project.handler
+            git_fetcher = Foy::GitFetcher.new(project.repository)
+            file = git_fetcher.get(handler.file)
+            caller = Foy::HandlerCaller.new(handler)
+            response = caller.parse(file)
+
+            project.project_packages.destroy
+
+            response.each_line do |line|
+              name, version = line.split(" ")
+              package = handler.packages.find_or_create_by(name: name)
+              package.save
+
+              project_package = project.project_packages.find_or_create_by(name: name)
+              project_package.version = version
+              project_package.save
+            end
+          end
+        end
+      end
+    end
+  end
+end
